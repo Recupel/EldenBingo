f18cf5e54c43d1000a6b330eff1c006a2cd8d88d
felixsoum <felix@felixsoum.com>
2022-04-09 02:48:12 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,41 +1,34 @@
 var goals = [
-"Rodrick",
-"Rennala",
-"Rykard",
-"Radahn",
-"Margit",
-"Deathroot",
-"Dragon Heart",
-"Wretched",
-"Arcane > 20",
-"Tree Sentinel",
-"Dectus Medallion",
-"Night Rider",
-"Black Beast",
-"Dexterity > 20",
-"Faith > 20",
-"Iron Cleaver",
-"Castle Morne",
-"Flask +3",
-"Any Big Bear",
-"Varre",
-"Death Bird",
-"Grafted Scion",
-"Giant Lobster",
-"Red Wolf",
-"Dragon Ruins Chest"
+  "Rodrick",
+  "Rennala",
+  "Rykard",
+  "Radahn",
+  "Margit",
+  "Deathroot",
+  "Dragon Heart",
+  "Wretched",
+  "Arcane > 20",
+  "Tree Sentinel",
+  "Dectus Medallion",
+  "Night Rider",
+  "Black Beast",
+  "Dexterity > 20",
+  "Faith > 20",
+  "Iron Cleaver",
+  "Castle Morne",
+  "Flask +3",
+  "Any Big Bear",
+  "Varre",
+  "Death Bird",
+  "Grafted Scion",
+  "Giant Lobster",
+  "Red Wolf",
+  "Dragon Ruins Chest"
 ];
 
-shuffle(goals);
-
-for (let i = 0; i < 25; i++) {
-	let text = i.toString();
-	//console.log(text);
-	document.getElementById(text).innerHTML = goals[i];
-}
-
 function shuffle(array) {
-  let currentIndex = array.length,  randomIndex;
+  let currentIndex = array.length,
+    randomIndex;
 
   // While there remain elements to shuffle...
   while (currentIndex != 0) {
@@ -46,8 +39,17 @@ function shuffle(array) {
 
     // And swap it with the current element.
     [array[currentIndex], array[randomIndex]] = [
-      array[randomIndex], array[currentIndex]];
+      array[randomIndex], array[currentIndex]
+    ];
   }
 
   return array;
+}
+
+shuffle(goals);
+
+for (let i = 0; i < 25; i++) {
+  let text = i.toString();
+  //console.log(text);
+  document.getElementById(text).innerHTML = goals[i];
 }
